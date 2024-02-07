0615481bafcce16dd0f2a73e13df74b23097e7dd
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-07 21:42:55 +0530

    add:smLinks Event

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const hamburger = document.getElementById("ham-menu");
 const hamburgerClose = document.getElementById("ham-menu-close");
 const menu = document.querySelector(".header__sm-links")
-//hamburger event
-hamburger.addEventListener("click", (e) => {
-    hamburger.style.display = "none";
-    hamburgerClose.style.display = "block"
-    menu.style.display = "block"
-})
-//hamburgerClose event
-hamburgerClose.addEventListener("click", () => {
+
+const activateHamburger = () => {
     hamburger.style.display = "block";
     hamburgerClose.style.display = "none"
     menu.style.display = "none"
+}
+const deActivateHamburger = () => {
+    hamburger.style.display = "none";
+    hamburgerClose.style.display = "block"
+    menu.style.display = "block"
+}
+//hamburger event
+hamburger.addEventListener("click", (e) => {
+    deActivateHamburger();
+})
+//hamburgerClose event
+hamburgerClose.addEventListener("click", () => {
+    activateHamburger();
+})
+//smLinks event
+const smLinks = document.querySelector(".header__sm-links");
+smLinks.addEventListener("click", (e) => {
+    if (e.target.closest("a").classList.contains("header__link")) {
+        activateHamburger();
+    }
 })
